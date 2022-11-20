6a47792e006e0048648d44e623222828d34c4442
Maykon Granemann <maykondgranemann@gmail.com>
2022-11-20 17:58:55 -0300

    Criado projeto inicial, Console e if/else

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Olá, mundo");
+
+            Console.Write("Digite seu nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Seja bem-vindo, " + nome);
+
+            Console.Write("Digite sua idade:");
+            int idade = Convert.ToInt32( Console.ReadLine() );
+            Console.WriteLine(nome + " sua idade é: " + idade);
+
+            //IF é uma estrutura de descisão
+            if (idade >= 18)
+            {
+                Console.WriteLine("Maior de idade");
+            }
+
+            if (idade < 18)
+            {
+                Console.WriteLine("Menor de idade");
+            }
+            Console.ReadLine();
+        }
+    }
+}
