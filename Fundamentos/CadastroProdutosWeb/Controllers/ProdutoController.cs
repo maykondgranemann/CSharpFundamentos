cab4994697db3f2ab2664c6e94229ca81e0bdf56
Carolina Hostin <carolinahostin1@gmail.com>
2022-12-04 18:26:49 -0300

    Cadastro Produto Web

---
+++ Fundamentos/CadastroProdutosWeb/Controllers/ProdutoController.cs
@@ -0,0 +1,37 @@
+﻿using CadastroProdutosWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroProdutosWeb.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public static List<Produto> ListaProdutos { get; set; } = new List<Produto>();
+
+        // GET, POST, PUT, DELETE
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet] // get - carregar tela/dados
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] // cadastrar/salvar
+        public IActionResult Create(Produto model)
+        {
+            ListaProdutos.Add(model);
+            ModelState.Clear();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return View(ListaProdutos);
+        }
+    }
+}
