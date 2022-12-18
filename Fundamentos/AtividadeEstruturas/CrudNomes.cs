005c0039fdd9c8ab5492e7c1da7d5efda5d84f95
Maykon Granemann <maykondgranemann@gmail.com>
2022-12-18 16:35:05 -0300

    CRUD e refatoracao

---
+++ Fundamentos/AtividadeEstruturas/CrudNomes.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeEstruturas
+{
+    internal class CrudNomes
+    {
+        List<string> Nomes = new List<string>();
+        public void Cadastrar()
+        {
+            Console.WriteLine("Cadastrando ...");
+            Console.Write("Digite um nome: ");
+            string nome = Console.ReadLine();
+            Nomes.Add(nome);
+        }
+        public void Listar()
+        {
+            Console.WriteLine("Listando Nomes ...");
+            foreach (string nome in Nomes)
+            {
+                Console.WriteLine(nome);
+            }
+        }
+        public void Alterar()
+        {
+            Console.WriteLine("Dados salvos:");
+            Listar();
+            Console.Write("Qual nome deseja alterar?");
+            string nomeAntigo = Console.ReadLine();
+            Console.Write("Digite o novo nome:");
+            string nomeNovo = Console.ReadLine();
+            Nomes.Remove(nomeAntigo);
+            Nomes.Add(nomeNovo);
+            Console.WriteLine("Dado alterado com sucesso!");
+        }
+        public void Alterar2()
+        {
+            Console.WriteLine("Dados salvos:");
+            int posicao = ListarPorPosicao();
+            Console.Write("Digite o novo nome:");
+            string nomeNovo = Console.ReadLine();
+            Nomes[posicao] = nomeNovo;
+            Console.WriteLine("Dado alterado com sucesso!");
+        }
+        public void Deletar()
+        {
+            Console.WriteLine("Deletando ...");
+            int posicao = ListarPorPosicao();
+            Nomes.RemoveAt(posicao);
+            Console.WriteLine("Dado deletado com sucesso!");
+        }
+
+        public int ListarPorPosicao()
+        {
+            for (int i = 0; i < Nomes.Count; i++)
+            {
+                Console.WriteLine($"{i} - {Nomes[i]}");
+            }
+            Console.Write("Digite o numero do dado a ser alterardo:");
+            int posicao = Convert.ToInt32(Console.ReadLine());
+            return posicao;
+        }
+    }
+}
---
+++ Fundamentos/AtividadeEstruturas/ViewTelaInicial.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AtividadeEstruturas
+{
+    internal class ViewTelaInicial
+    {
+        CrudNomes crudNomes = new CrudNomes();
+        public void ImprimirMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("========== Cadastro de pessoas ==========\n");
+            Console.WriteLine("\t1 - Cadastrar");
+            Console.WriteLine("\t2 - Listar");
+            Console.WriteLine("\t3 - Alterar");
+            Console.WriteLine("\t4 - Deletar");
+            Console.Write("\nEscolha uma das opçÕes do menu:");
+        }
+        public byte LerOpcao()
+        {
+            byte opcao = 0;
+            try
+            {
+                opcao = Convert.ToByte(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Dado invalido!");
+                Thread.Sleep(1500);
+            }
+            return opcao;
+        }
+        public void EscolherOpcao(byte opcao)
+        {
+            
+            switch (opcao)
+            {
+                case 1:
+                    crudNomes.Cadastrar();
+                    break;
+                case 2:
+                    crudNomes.Listar();
+                    break;
+                case 3:
+                    crudNomes.Alterar2();
+                    break;
+                case 4:
+                    crudNomes.Deletar();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    Thread.Sleep(1500);
+                    break;
+            }
+        }
+
+        public bool PerguntarNovaOperacao(byte opcao)
+        {
+            bool continua=false;
+
+            if (opcao > 0 && opcao < 5)
+            {
+                Console.Write("Deseja realizar outra operação?(S/N)");
+                string opcaoContinuar = Console.ReadLine();
+                if (opcaoContinuar.ToUpper() == "S")
+                {
+                    continua = true;
+                }
+                else
+                {
+                    continua = false;
+                }
+            }
+            return continua;
+        }
+    }
+}
